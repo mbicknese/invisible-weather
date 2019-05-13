c4c5ab5ba26adf1036a9c4d45efa06b64521e778
Maarten Bicknese <maarten.bicknese@gmail.com>
2019-05-13 11:24:41 +0200

    Rewrite weather to encompass coordinate retrieval

--- src/weather/index.ts
+++ src/weather/index.ts
@@ -1,40 +1,12 @@
 import { isUSZip } from '../utils'
 import { getOpenWeatherDataByQuery, getOpenWeatherDataByZip } from './api'
-
-export const getWeather = async (location: string): Promise<ICurrentWeather> => {
-    if (isUSZip(location)) {
-        return getCurrentWeatherForZip(location)
-    }
-    return getCurrentWeatherForQuery(location)
-}
-
-
-const extractCurrentWeather = (data: IOpenWeatherObject): ICurrentWeather => ({
-    main: data.weather[0].main,
-    description: data.weather[0].description,
-})
-const getCurrentWeather = async (retrieve: () => Promise<IOpenWeatherObject>): Promise<ICurrentWeather> => {
-    try {
-        const data = await retrieve()
-        return extractCurrentWeather(data)
-    } catch (error) {
-        if (error === 'city not found') {
-            throw new WeatherLocationNotFoundError()
-        }
-        return { main: 'Unknown', description: 'unknown' }
-    }
-}
-const getCurrentWeatherForQuery = async (query: string): Promise<ICurrentWeather> => getCurrentWeather(() => getOpenWeatherDataByQuery(query))
-const getCurrentWeatherForZip = async (zip: string): Promise<ICurrentWeather> => getCurrentWeather(() => getOpenWeatherDataByZip(zip))
-
-export const getLatLong = async (location: string): Promise<{ lat: string, long: string }> => {
-    return { lat: '', long: '' }
-}
+import unknownWeather from './unknownWeather.json'
 
 export interface ICurrentWeather {
     main: string,
     description: string
 }
+export interface ILatLong { lat: number|null, long: number|null }
 
 export class WeatherLocationNotFoundError extends Error {
     constructor() {
@@ -44,3 +16,46 @@ export class WeatherLocationNotFoundError extends Error {
         Object.setPrototypeOf(this, WeatherLocationNotFoundError.prototype)
     }
 }
+
+type WeatherDataExtractor<ReturnType> = (data: IOpenWeatherObject) => ReturnType
+const retrieveAndExtract = <R>(extract: WeatherDataExtractor<R>) => async (retrieve: () => Promise<IOpenWeatherObject>): Promise<R> => {
+    try {
+        const data = await retrieve()
+        return extract(data)
+    } catch (error) {
+        if (error === 'city not found') {
+            throw new WeatherLocationNotFoundError()
+        }
+        return extract(unknownWeather)
+    }
+}
+
+type LocationBasedRetriever<ReturnType> = (location: string) => Promise<ReturnType>
+const locationSwitch = <R>(onZip: LocationBasedRetriever<R>, onQuery: LocationBasedRetriever<R>) =>
+    async (location: string): Promise<R> => {
+        if (isUSZip(location)) {
+            return onZip(location)
+        }
+        return onQuery(location)
+    }
+
+
+// Current Weather composition
+const extractCurrentWeather = (data: IOpenWeatherObject): ICurrentWeather => ({
+    main: data.weather[0].main,
+    description: data.weather[0].description,
+})
+const getCurrentWeather = retrieveAndExtract(extractCurrentWeather)
+const getCurrentWeatherForQuery = async (query: string): Promise<ICurrentWeather> => getCurrentWeather(() => getOpenWeatherDataByQuery(query))
+const getCurrentWeatherForZip = async (zip: string): Promise<ICurrentWeather> => getCurrentWeather(() => getOpenWeatherDataByZip(zip))
+export const getWeather = locationSwitch(getCurrentWeatherForZip, getCurrentWeatherForQuery)
+
+// Latitude and Longitude composition
+const extractCoords = (data: IOpenWeatherObject): ILatLong => ({
+    lat: data.coord.lat,
+    long: data.coord.lon
+})
+const getCoords = retrieveAndExtract(extractCoords)
+const getLatLongForQuery = async (query: string): Promise<ILatLong> => getCoords(() => getOpenWeatherDataByQuery(query))
+const getLatLongForZip = async (zip: string): Promise<ILatLong> => getCoords(() => getOpenWeatherDataByZip(zip))
+export const getLatLong = locationSwitch(getLatLongForZip, getLatLongForQuery)
